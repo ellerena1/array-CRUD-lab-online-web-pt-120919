3e6cbc6329b8b7a4916f349e67903cdb7e3a3ea6
Esteban S Llerena <estebanllerena@gmail.com>
2019-12-20 03:13:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  student = ["wow", "Jocelyn", "Nick", "Esteban", "Alan", "arrays!"]
+  student = ["wow", "Jocelyn", "Nick", "Esteban", "Alan", "arrays!", "am"]
+  student = [6]
  
 end
 
